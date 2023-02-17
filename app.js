d29645503d1f24bbfaeae5cccb2315a423e8f8a4
Ajinkya Bhilare <ajinkya0011@gmail.com>
2023-02-17 20:05:41 +0530

    level2 encription

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
+const encrypt = require("mongoose-encryption");
 const ejs = require("ejs");
 const app = express();
 app.use(express.static("public"));
@@ -11,10 +11,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.set("strictQuery", true);
 mongoose.connect("mongodb://127.0.0.1:27017/userDB", { useNewUrlParser: true });
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-};
+});
+const secret = "wearebetterinpasswordmanagement";
+userSchema.plugin(encrypt, { secret: secret,encryptedFields:['password'] });
 const User = new mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
